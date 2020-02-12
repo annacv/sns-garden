677c99f7074daf1c022d3e29540344cbdecea325
acondal <anna@lacupulamusic.com>
2020-02-12 17:33:18 +0100

    Add dynamic layouts

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <component :is="layout" id="app" class="app">
+    <router-view :layout.sync="layout"/>
+  </component>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import '@/assets/scss/app.scss'
+import LayoutDefault from '@/layouts/LayoutDefault.vue'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  data () {
+    return {
+      layout: 'layout-default'
+    }
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  components: {
+    'layout-default': LayoutDefault
+  }
 }
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="SonoSuite logo" src="../assets/logo.png" class="app__brand">
+    <HelloWorld :msg="$t('hello')"/>
+    <h2>{{ $t('hello') }}</h2>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import LayoutHello from '@/layouts/LayoutHello.vue'
 
 export default {
   name: 'Home',
+
   components: {
     HelloWorld
+  },
+
+  created () {
+    this.$emit('update:layout', LayoutHello)
   }
 }
 </script>
