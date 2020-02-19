36b210be0bb6a6bc1712169cd0da28cd59cb5ce9
acondal <anna@lacupulamusic.com>
2020-02-19 12:51:11 +0100

    Store mutations and getters for steps and userData

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,53 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    wizard: {
+      step: 1
+    },
+    userData: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      password: '',
+      hashedPassword: ''
+    }
   },
+
   mutations: {
+  /**
+  * Set wizard steps
+  * @param state
+  * @param step
+  */
+    setStep (state, step) {
+      state.wizard.step = step
+    },
+    /**
+  * Update userData
+  * @param state
+  * @param userData
+  */
+    setUserData (state, userData) {
+      console.log(userData)
+      const values = Object.keys(userData)
+      values.forEach((key, n) => {
+        state.userData[values[n]] = userData[key]
+      })
+    }
   },
+
   actions: {
   },
+
+  getters: {
+    getStep (state) {
+      return state.wizard.step
+    },
+    getUser (state) {
+      return state.userData
+    }
+  },
+
   modules: {
   }
 })
